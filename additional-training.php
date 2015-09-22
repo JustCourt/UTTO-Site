e388bcbb233fc58565153e1a57bc401052739e2b
JustCourt <courtney.j.siu@gmail.com>
2015-09-22 00:37:13 -0400

    changed html files to php files

---
+++ additional-training.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+	<head>
+		<title>UTTO - Additional Training</title>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css">
+		<link type="image/jpg" rel="icon" href="images/icon.jpg"/>
+		<meta name="keywords" content="UTTO, UofT, Taekwondo, Hart House, University of Toronto">
+		<meta name="description" content="Visit to learn about the University of Toronto Taekwondo Organization">
+		<meta name="author" content="UTTO">
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<meta name=viewport content="width=device-width, initial-scale=1">
+
+		<!--FlexSlider Plugin for Slideshow-->
+		<link rel="stylesheet" href="flexslider.css" type="text/css">
+		<script src="jquery.flexslider.js"></script>
+		<script type="text/javascript" charset="utf-8">
+		$(window).load(function() {
+		  $('.flexslider').flexslider({
+		    animation: "slide",
+		    controlNav: "thumbnails"
+		  });
+		});
+		</script>
+		<!--End FlexSlider Plugin-->
+
+	</head>
+	<body>
+		<!--Navbar-->
+		<div id="navbar">
+			<ul>
+				<li id="site-title"><a href='index.html' >UNIVERSITY OF TORONTO TAEKWONDO ORGANIZATION</a></li>
+				<li><a href='About.html'>ABOUT US</a>
+					<ul>
+						<li><a href="exec-team.html">EXECUTIVE TEAM</a></li>
+					</ul>
+				</li>
+				<li><a href='classes.html'>CLASSES</a>
+					<ul>
+						<li><a class="active" href="additional-training.html">ADDITIONAL TRAINING</a></li>
+					</ul>
+				</li>
+				<li><a href='events.html'>EVENTS</a>
+					<ul>
+						<li><a href="past-events.html">PAST EVENTS</a></li>
+					</ul>
+				</li>
+				<li><a href='competitive-team.html'>TEAM</a></li>
+				<li><a href='Contact.html'>CONTACT</a></li>
+				<li id="icon-bar"><a href="http://instagram.com/uofttaekwondo?ref=badge"><img src="images/instagram.png" height=24px/></a> <a href="https://www.facebook.com/uofttaekwondo"> <img src="images/facebook.png" height=24px/> </a>
+					<a href="https://www.youtube.com/channel/UCwNyp8-2Q7SFhThkkLDNOQg/feed"><img src="images/youtube.png" height=24px/></a>
+				</li>
+			</ul>
+		</div>
+		<!--End Navbar-->
+
+		<h1>ADDITIONAL TRAINING</h1>
+		<div class="content">
+			<p>UTTO often works with other universities and local Taekwondo clubs to host joint training sessions. These training sessions are usually held off campus and are additional to competitive team practices. We also have regular fitness and poomsae sessions scheduled for the year. All training sessions are open to anyone enrolled in a class.</p>
+			<h6>Schedule</h6>
+			<h5>Fitness Sessions</h5>
+			<p class="event_info">
+				Every Tuesday from 8-9, starting Sept. 29<br/>
+				Hart House<br/>
+			</p>
+			<h5>Poomsae Sessions</h5>
+			<p>More information TBA</p>
+			<h5>Testing</h5>
+			<p>We will be offering belt testing this year in February. More information TBA</p>
+			<br/>
+			<p>Check out our gallery below for pictures from some of our additional training sessions!</p>
+			
+			<!-- Slideshow -->
+			<div class="flexslider">
+				<ul class="slides">
+					<li data-thumb="images/addtraining1.jpg">
+						<img src="images/addtraining1.jpg">
+					</li>
+					<li data-thumb="images/addtraining2.jpg">
+						<img src="images/addtraining2.jpg">
+					</li>
+					<li data-thumb="images/addtraining3.jpg">
+						<img src="images/addtraining3.jpg">
+					</li>
+					<li data-thumb="images/addtraining4.jpg">
+						<img src="images/addtraining4.jpg">
+					</li>
+				</ul>
+			</div>
+			<!-- End Slideshow -->
+		
+		</div>
+		<div class="footer">
+			<small>
+			Copyright &copy 
+			<script>
+			var d= new Date()
+			document.write(d.getFullYear())
+			</script> 
+			UTTO
+			</small>
+		</div>
+	</body>
+</html>
---
+++ competitive-team.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+	<head>
+		<title>UTTO - Competitive Team</title>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css">
+		<link type="image/jpg" rel="icon" href="images/icon.jpg"/>
+		<meta name="keywords" content="UTTO, UofT, Taekwondo, Hart House, University of Toronto">
+		<meta name="description" content="Visit to learn about the University of Toronto Taekwondo Organization">
+		<meta name="author" content="UTTO">
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<meta name=viewport content="width=device-width, initial-scale=1">
+
+		<!--FlexSlider Plugin for Slideshow-->
+		<link rel="stylesheet" href="flexslider.css" type="text/css">
+		<script src="jquery.flexslider.js"></script>
+		<script type="text/javascript" charset="utf-8">
+		$(window).load(function() {
+		  $('.flexslider').flexslider({
+		    animation: "slide",
+		    controlNav: "thumbnails"
+		  });
+		});
+		</script>
+		<!--End FlexSlider Plugin-->
+	
+	</head>
+	<body>
+
+		<!--Navbar-->
+		<div id="navbar">
+			<ul>
+				<li id="site-title"><a href='index.html' >UNIVERSITY OF TORONTO TAEKWONDO ORGANIZATION</a></li>
+				<li><a href='About.html'>ABOUT US</a>
+					<ul>
+						<li><a href="exec-team.html">EXECUTIVE TEAM</a></li>
+					</ul>
+				</li>
+				<li><a href='classes.html'>CLASSES</a>
+					<ul>
+						<li><a href="additional-training.html">ADDITIONAL TRAINING</a></li>
+					</ul>
+				</li>
+				<li><a href='events.html'>EVENTS</a>
+					<ul>
+						<li><a href="past-events.html">PAST EVENTS</a></li>
+					</ul>
+				</li>
+				<li><a href='competitive-team.html'>TEAM</a></li>
+				<li><a href='Contact.html'>CONTACT</a></li>
+				<li id="icon-bar"><a href="http://instagram.com/uofttaekwondo?ref=badge"><img src="images/instagram.png" height=24px/></a> <a href="https://www.facebook.com/uofttaekwondo"> <img src="images/facebook.png" height=24px/> </a>
+					<a href="https://www.youtube.com/channel/UCwNyp8-2Q7SFhThkkLDNOQg/feed"><img src="images/youtube.png" height=24px/></a>
+				</li>
+			</ul>
+		</div>
+		<!--End Navbar-->
+
+		<h1>COMPETITIVE TEAM</h1>
+		<div class="content">
+			<p>UTTO's competitive team represents the University of Toronto by competing at local tournaments sanctioned by the Ontario Taekwondo Association. The team is open to all belt-levels, though commitment is required.</p>
+			<p>You can sign up through Hart House at the link below.</p>
+			<p>Before competing, you must sign up for an <a href="http://www.taekwondo.on.ca/index.php/administration/forms-documents/">OTA</a> membership.
+			<h6>Tournament Dates</h6>
+			<h5>Toronto Open Taekwondo Championships</h5>
+			<p class="event_info">
+				November 14-15, 2015
+			</p>
+			<h5>Sport TKD Open</h5>
+			<p class="event_info">
+				March 5-6, 2016
+			</p>
+
+			<br/>
+			<h6>COMPETITIVE TRAINING PRACTICES</h6>
+			<p class="event_info">
+				Saturday 6:10 - 8pm and Wednesday 10pm - 11pm (Tentative)<br/>
+				Sept. 23 - Dec. 05<br/>
+				Hart House - Activities Room<br/>
+				<a href="http://harthouse.ca/eventssingle/tae-kwon-do-competitive-training-group_5981">More Info and Registration</a>
+			</p>
+			<p>Check out our gallery below for pictures of our competitive team in action at tournaments & training (and slacking off).</p>
+			
+			<!-- Slideshow -->
+			<div class="flexslider">
+				<ul class="slides">
+					<li data-thumb="images/compteam1.jpg">
+						<img src="images/compteam1.jpg">
+					</li>
+					<li data-thumb="images/compteam2.jpg">
+						<img src="images/compteam2.jpg">
+					</li>
+					<li data-thumb="images/compteam3.jpg">
+						<img src="images/compteam3.jpg">
+					</li>
+					<li data-thumb="images/compteam4.jpg">
+						<img src="images/compteam4.jpg">
+					</li>
+					<li data-thumb="images/compteam5.jpg">
+						<img src="images/compteam5.jpg">
+					</li>
+					<li data-thumb="images/compteam6.jpg">
+						<img src="images/compteam6.jpg">
+					</li>
+					<li data-thumb="images/compteam7.jpg">
+						<img src="images/compteam7.jpg">
+					</li>
+					<li data-thumb="images/compteam8.jpg">
+						<img src="images/compteam8.jpg">
+					</li>
+					<li data-thumb="images/compteam9.jpg">
+						<img src="images/compteam9.jpg">
+					</li>
+				</ul>
+			</div>
+			<!-- End Slideshow -->
+
+		</div>
+		<div class="footer">
+			<small>
+			Copyright &copy 
+			<script>
+			var d= new Date()
+			document.write(d.getFullYear())
+			</script> 
+			UTTO
+			</small>
+		</div>
+	</body>
+</html>
